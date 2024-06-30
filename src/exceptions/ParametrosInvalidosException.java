7be20292a3583435bc0051b159b4abc796bb4fc2
kaue_ <kauepatrick125@gmail.com>
2024-06-29 21:03:25 -0300

    Desafio controleFluxo

---
+++ src/exceptions/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class ParametrosInvalidosException extends Exception{
+    public ParametrosInvalidosException(String error){
+        System.out.println();
+
+    }
+}
